93993405743f8fcb6f1297d78dd6be3ac56ccd3b
William Bruntrager <w.bruntrager@gmail.com>
2016-12-26 14:47:10 -0500

    very clean, first commit

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import os
 
 import jinja2
@@ -22,6 +8,9 @@ import webapp2
 from google.appengine.ext import ndb
 
 import json
+import time
+
+import logging
 
 class Animal(ndb.Model):
     name = ndb.StringProperty()
@@ -42,11 +31,44 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.response.out.write(render_str(template, **kw))
 
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        if self.request.url.endswith('.json'):
+            self.format = 'json'
+        else:
+            self.format = 'html'
+
 class MainHandler(Handler):
     def get(self):
         # this is the line you will change
         self.render('main.html')
 
+class GreetingHandler(Handler):
+    def get(self, name):
+        self.write("Hello, %s" % (name))
+
+class AnimalHandler(Handler):
+    def get(self):
+        # import pdb; pdb.set_trace()
+        animals = Animal.query().fetch()
+        if self.format == "json":
+            o = []
+            for animal in animals:
+                o.append({"name": animal.name })
+            msg = json.dumps(o)
+            self.response.headers.add_header('Content-Type', "application/json")
+            self.write(msg)
+        else:
+            self.render('animals.html', animals = animals)
+    def post(self):
+        name = self.request.get('name')
+        animal = Animal(name = name)
+        animal.put()
+        time.sleep(.5)
+        self.redirect('/animals')
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/greet/(.*)', GreetingHandler),
+    ('/animals(?:.json)?', AnimalHandler)
 ], debug=True)
